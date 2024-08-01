7e27eca96d92cc966748e66a46798df56d6126a5
Azalea <2574292235@qq.com>
2024-08-01 15:48:45 +0800

    测试

--- src/ch2b_hello_world.c
+++ src/ch2b_hello_world.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/// 正确输出：
-/// Hello world from user mode program!
-/// Test hello_world OK!
+// 正确输出：
+// Hello world from user mode program!
+// Test hello_world OK!
 
 int main()
 {
